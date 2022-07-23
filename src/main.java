ee78edd655ffc2accf9d924c0e35454f4b239944
nburavtsov <niburavtsov@gmail.com>
2022-07-23 17:41:39 +0300

    New commit

--- src/main.java
+++ src/main.java
@@ -1,7 +1,7 @@
 public class main {
     public static void main(String[] args) {
 
-        int ticketPrice = 15_000;
+        int ticketPrice = 10000;
         int milePrice = 20;
         int milesEarned = ticketPrice / milePrice;
         System.out.println(milesEarned);
